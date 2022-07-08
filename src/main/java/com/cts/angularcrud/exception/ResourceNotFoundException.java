fac5f57107d69c0cbd31e6967f53063ead8c7eb3
PriyajitRath <92915944+PriyajitRath@users.noreply.github.com>
2022-07-08 09:23:22 +0530

    Add files via upload
    all updated code

---
+++ src/main/java/com/cts/angularcrud/exception/ResourceNotFoundException.java
@@ -0,0 +1,19 @@
+package com.cts.angularcrud.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException  extends RuntimeException{
+
+	
+	
+	private static final long serialversionUID = 1l;
+	
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+	
+}
